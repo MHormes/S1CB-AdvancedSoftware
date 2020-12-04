3d0b3ac6861eccc125365aa34264fb3e0d5835c2
Maarten <maarten.hormes@gmail.com>
2020-12-04 11:35:48 +0100

    Made the assignment

--- TimerTest/TimerTest/Form1.Designer.cs
+++ TimerTest/TimerTest/Form1.Designer.cs
@@ -30,12 +30,54 @@ namespace TimerTest
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnStopStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(12, 9);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 55);
+            this.lblTimer.TabIndex = 0;
+            // 
+            // btnStopStart
+            // 
+            this.btnStopStart.Location = new System.Drawing.Point(22, 101);
+            this.btnStopStart.Name = "btnStopStart";
+            this.btnStopStart.Size = new System.Drawing.Size(146, 72);
+            this.btnStopStart.TabIndex = 1;
+            this.btnStopStart.Text = "Start/Stop";
+            this.btnStopStart.UseVisualStyleBackColor = true;
+            this.btnStopStart.Click += new System.EventHandler(this.btnStopStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStopStart);
+            this.Controls.Add(this.lblTimer);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnStopStart;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- TimerTest/TimerTest/Form1.cs
+++ TimerTest/TimerTest/Form1.cs
@@ -12,9 +12,24 @@ namespace TimerTest
 {
     public partial class Form1 : Form
     {
+        bool isOnOrNot = true;
+        int timePassed = 0;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timePassed += 1;
+            lblTimer.Text = Convert.ToString(timePassed);
+        }
+
+        private void btnStopStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = isOnOrNot;
+            isOnOrNot = !isOnOrNot;
         }
     }
 }
