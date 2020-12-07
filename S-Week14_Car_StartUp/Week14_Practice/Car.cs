38c5117cbe6aa956c15f5c8cc4721a294f880a11
Maarten <maarten.hormes@gmail.com>
2020-12-07 15:58:00 +0100

    update

---
+++ S-Week14_Car_StartUp/Week14_Practice/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week14_Practice
+{
+    class Car
+    {
+        private string model;
+        private int topSpeed;
+        private int speed;
+
+
+    }
+}
